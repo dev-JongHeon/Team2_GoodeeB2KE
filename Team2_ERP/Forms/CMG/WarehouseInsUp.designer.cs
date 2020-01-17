5446f61111edbe4224c1e5b0b54441b3b0c1ce91
서선도 <tjtjseh1234@naver.com>
2020-01-17 14:59:26 +0900

    선도

--- Team2_ERP/Forms/CMG/WarehouseInsUp.designer.cs
+++ Team2_ERP/Forms/CMG/WarehouseInsUp.designer.cs
@@ -35,10 +35,10 @@
             this.label5 = new System.Windows.Forms.Label();
             this.txtWarehouseCode = new System.Windows.Forms.TextBox();
             this.txtWarehouseName = new System.Windows.Forms.TextBox();
-            this.txtWarehouseDivision = new System.Windows.Forms.TextBox();
             this.txtWarehouseNumber = new System.Windows.Forms.TextBox();
             this.txtWarehouseFaxNumber = new System.Windows.Forms.TextBox();
             this.addressControl1 = new Team2_ERP.AddressControl();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.panel1.SuspendLayout();
             this.panel_Title.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbxTitle)).BeginInit();
@@ -49,10 +49,10 @@
             // 
             // panel5
             // 
+            this.panel5.Controls.Add(this.comboBox1);
             this.panel5.Controls.Add(this.addressControl1);
             this.panel5.Controls.Add(this.txtWarehouseFaxNumber);
             this.panel5.Controls.Add(this.txtWarehouseNumber);
-            this.panel5.Controls.Add(this.txtWarehouseDivision);
             this.panel5.Controls.Add(this.txtWarehouseName);
             this.panel5.Controls.Add(this.txtWarehouseCode);
             this.panel5.Controls.Add(this.label5);
@@ -72,7 +72,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(49, 22);
+            this.label1.Location = new System.Drawing.Point(346, 47);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(51, 14);
             this.label1.TabIndex = 0;
@@ -81,7 +81,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(49, 73);
+            this.label2.Location = new System.Drawing.Point(81, 44);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(51, 14);
             this.label2.TabIndex = 0;
@@ -90,7 +90,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(49, 128);
+            this.label3.Location = new System.Drawing.Point(81, 99);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(29, 14);
             this.label3.TabIndex = 0;
@@ -99,7 +99,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(49, 182);
+            this.label4.Location = new System.Drawing.Point(81, 153);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(51, 14);
             this.label4.TabIndex = 0;
@@ -108,7 +108,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(49, 234);
+            this.label5.Location = new System.Drawing.Point(81, 205);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(51, 14);
             this.label5.TabIndex = 0;
@@ -116,35 +116,28 @@
             // 
             // txtWarehouseCode
             // 
-            this.txtWarehouseCode.Location = new System.Drawing.Point(126, 19);
+            this.txtWarehouseCode.Location = new System.Drawing.Point(423, 44);
             this.txtWarehouseCode.Name = "txtWarehouseCode";
             this.txtWarehouseCode.Size = new System.Drawing.Size(143, 21);
             this.txtWarehouseCode.TabIndex = 1;
             // 
             // txtWarehouseName
             // 
-            this.txtWarehouseName.Location = new System.Drawing.Point(126, 70);
+            this.txtWarehouseName.Location = new System.Drawing.Point(158, 41);
             this.txtWarehouseName.Name = "txtWarehouseName";
             this.txtWarehouseName.Size = new System.Drawing.Size(143, 21);
             this.txtWarehouseName.TabIndex = 1;
             // 
-            // txtWarehouseDivision
-            // 
-            this.txtWarehouseDivision.Location = new System.Drawing.Point(126, 125);
-            this.txtWarehouseDivision.Name = "txtWarehouseDivision";
-            this.txtWarehouseDivision.Size = new System.Drawing.Size(143, 21);
-            this.txtWarehouseDivision.TabIndex = 1;
-            // 
             // txtWarehouseNumber
             // 
-            this.txtWarehouseNumber.Location = new System.Drawing.Point(126, 179);
+            this.txtWarehouseNumber.Location = new System.Drawing.Point(158, 150);
             this.txtWarehouseNumber.Name = "txtWarehouseNumber";
             this.txtWarehouseNumber.Size = new System.Drawing.Size(143, 21);
             this.txtWarehouseNumber.TabIndex = 1;
             // 
             // txtWarehouseFaxNumber
             // 
-            this.txtWarehouseFaxNumber.Location = new System.Drawing.Point(126, 227);
+            this.txtWarehouseFaxNumber.Location = new System.Drawing.Point(158, 198);
             this.txtWarehouseFaxNumber.Name = "txtWarehouseFaxNumber";
             this.txtWarehouseFaxNumber.Size = new System.Drawing.Size(143, 21);
             this.txtWarehouseFaxNumber.TabIndex = 1;
@@ -153,10 +146,18 @@
             // 
             this.addressControl1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.addressControl1.Font = new System.Drawing.Font("나눔고딕", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.addressControl1.Location = new System.Drawing.Point(52, 270);
+            this.addressControl1.Location = new System.Drawing.Point(84, 243);
             this.addressControl1.Name = "addressControl1";
             this.addressControl1.Size = new System.Drawing.Size(505, 106);
             this.addressControl1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(158, 91);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(143, 22);
+            this.comboBox1.TabIndex = 3;
             // 
             // WarehouseInsUp
             // 
@@ -185,9 +186,9 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox txtWarehouseFaxNumber;
         private System.Windows.Forms.TextBox txtWarehouseNumber;
-        private System.Windows.Forms.TextBox txtWarehouseDivision;
         private System.Windows.Forms.TextBox txtWarehouseName;
         private System.Windows.Forms.TextBox txtWarehouseCode;
         private AddressControl addressControl1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
