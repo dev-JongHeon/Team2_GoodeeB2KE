5219d6206e672e2ca910870954f5dc376a489ead
dev-JongHeon <redmoon1510@gmail.com>
2020-01-20 14:26:36 +0900

    2 26

--- Team2_POP/Program.cs
+++ Team2_POP/Program.cs
@@ -14,6 +14,7 @@ namespace Team2_POP
         [STAThread]
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new PopMain());
