8c596eccbf1d39aa0b6e5ae87a6ee3e14ce806eb
esp7789 <gjwlstjd203@naver.com>
2020-01-20 13:01:45 +0900

    dac단 추가

---
+++ Team2_VO/HJS/Performance.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team2_VO
+{
+    public class Performance
+    {
+        public string Performance_ID { get; set; }
+        public string Produce_ID { get; set; }
+        public int Performance_QtySuccessItem { get; set; }
+        public int Performance_QtyDefectiveItem { get; set; }
+        public DateTime Performance_StartDate { get; set; }
+        public DateTime Performance_EndDate { get; set; }
+        public int Employees_ID { get; set; }
+        public string Employees_Name { get; set; }
+        public float Performance_DefectiveRate { get; set; }
+        public string Performance_ElapsedTime { get; set; }
+    }
+}
