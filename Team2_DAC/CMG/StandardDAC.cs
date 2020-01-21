2ebd27974e543467acdf3435cde239827733f365
car2300 <audrms03@naver.com>
2020-01-21 14:39:47 +0900

    최명근_Resource DAC, VO, Form에 코드 추가

---
+++ Team2_DAC/CMG/StandardDAC.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Team2_VO;
+
+namespace Team2_DAC
+{
+    public class StandardDAC : ConnectionInfo
+    {
+        SqlConnection conn = null;
+
+        public StandardDAC()
+        {
+            conn = new SqlConnection();
+            conn.ConnectionString = this.ConnectionString;
+        }
+
+        public List<ResourceVO> GetAllResource()
+        {
+            string sql = "select Product_ID, Product_Name, Warehouse_ID, Product_Price, Product_Qty, Product_Safety, Product_DeletedYN, Product_Category from Product ";
+
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    conn.Open();
+                    List<ResourceVO> list = Helper.DataReaderMapToList<ResourceVO>(cmd.ExecuteReader());
+                    return list;
+                }
+            }
+            catch (Exception err)
+            {
+                throw new Exception(err.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
---
+++ Team2_ERP/Service/CMG/StandardService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Team2_DAC;
+using Team2_VO;
+
+namespace Team2_ERP.Service.CMG
+{
+    public class StandardService
+    {
+        public List<ResourceVO> GetAllResource()
+        {
+            StandardDAC dac = new StandardDAC();
+            return dac.GetAllResource();
+        }
+    }
+}
