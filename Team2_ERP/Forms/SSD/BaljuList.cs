ac2d6d8473f59c7f02b871407982ddc43adc7631
서선도 <tjtjseh1234@naver.com>
2020-01-30 11:52:35 +0900

    선도

--- Team2_ERP/Forms/SSD/BaljuList.cs
+++ Team2_ERP/Forms/SSD/BaljuList.cs
@@ -35,28 +35,22 @@ namespace Team2_ERP
         private void LoadData()
         {
             UtilClass.SettingDgv(dgv_Balju);
-            UtilClass.AddNewColum(dgv_Balju, "발주지시번호", "Balju_ID", true);
-            UtilClass.AddNewColum(dgv_Balju, "거래처코드", "Company_ID", true);
-            UtilClass.AddNewColum(dgv_Balju, "거래처명칭", "Company_Name", true);
-            UtilClass.AddNewColum(dgv_Balju, "발주요청일시", "Balju_Date", true);
-            UtilClass.AddNewColum(dgv_Balju, "등록사원", "Employees_Name", true);
+            //dgv_Balju.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            UtilClass.AddNewColum(dgv_Balju, "발주지시번호", "Balju_ID", true, 130);
+            UtilClass.AddNewColum(dgv_Balju, "거래처코드", "Company_ID", true, 110);
+            UtilClass.AddNewColum(dgv_Balju, "거래처명칭", "Company_Name", true, 500);
+            UtilClass.AddNewColum(dgv_Balju, "발주요청일시", "Balju_Date", true, 140);
+            UtilClass.AddNewColum(dgv_Balju, "등록사원", "Employees_Name", true, 100);
             UtilClass.AddNewColum(dgv_Balju, "삭제여부", "Balju_DeletedYN", false);
-            dgv_Balju.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            dgv_Balju.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
-            dgv_Balju.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
-            dgv_Balju.Columns[4].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+
             Balju_AllList = service.GetBaljuList();  // 발주리스트 갱신
             dgv_Balju.DataSource = Balju_AllList;
 
             UtilClass.SettingDgv(dgv_BaljuDetail);
-            UtilClass.AddNewColum(dgv_BaljuDetail, "발주지시번호", "Balju_ID", true);
-            UtilClass.AddNewColum(dgv_BaljuDetail, "품목코드", "Product_ID", true);
-            UtilClass.AddNewColum(dgv_BaljuDetail, "품목명", "Product_Name", true);
-            UtilClass.AddNewColum(dgv_BaljuDetail, "발주요청수량", "BaljuDetail_Qty", true);
-            dgv_BaljuDetail.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            dgv_BaljuDetail.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
-            dgv_BaljuDetail.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
-            dgv_BaljuDetail.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            UtilClass.AddNewColum(dgv_BaljuDetail, "발주지시번호", "Balju_ID", true, 130);
+            UtilClass.AddNewColum(dgv_BaljuDetail, "품목코드", "Product_ID", true, 100);
+            UtilClass.AddNewColum(dgv_BaljuDetail, "품목명", "Product_Name", true, 500);
+            UtilClass.AddNewColum(dgv_BaljuDetail, "발주요청수량", "BaljuDetail_Qty", true, 130);
             BaljuDetail_AllList = service.GetBalju_DetailList(); // 발주디테일 AllList 갱신
 
         }
