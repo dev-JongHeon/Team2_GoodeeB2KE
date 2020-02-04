b6020adda805d395081adbdce7f58e56af804187
esp7789 <gjwlstjd203@naver.com>
2020-02-04 18:22:41 +0900

    -- 1순위 <보류> 생산시작 버튼 누를경우 => WaitingForm
    -- 2순위 <실적 선택시>
    불량처리버튼 오류 수정
    
    -- 3순위 <catch절>
    로그4넷 기록작업
    리소스 string
    
    --4순위<open.form 관련>
    생산시작 컬렉션 오류 수정
    
    --5순위<프로시저 수정>
    작업종료시 => 작업중이 들어가는 버그
    
    --6순위 유효성 검사 (콤보박스 선택 여부)
    
    (수) => 오늘못한거 + 모니터링
    (목) => 홈페이지 1 (회원가입, Session)
    (금) => 홈페이지 2 (상품 페이지, 카트)
    (토) => 홈페이지 3 (구매 페이지, 결제)
    (일) => 홈페이지 4 (주문현황, 배송현황)
    (월) => 홈페이지 5 (Mypage)

--- Team2_Machine/Service/Service.cs
+++ Team2_Machine/Service/Service.cs
@@ -9,11 +9,13 @@ namespace Team2_Machine
 {
     public class Service
     {
+        // 생산 진행
         public void Producing(string performanceID, int success, int bad)
         {
             new POPDAC().InProduction(performanceID, success, bad);
         }
 
+        // 생산 종료
         public void EndProduce(string performanceID)
         {
             new POPDAC().EndProduce(performanceID);
