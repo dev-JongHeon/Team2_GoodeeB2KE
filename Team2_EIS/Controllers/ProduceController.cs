b33c3bba3269588cb7ef5177bdffe680de24d2a9
종헌 김 <bluemoon1510@naver.com>
2020-02-11 01:30:42 +0900

    김종헌 자기전 adminlte 필요없는부분 삭제

---
+++ Team2_EIS/Controllers/ProduceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Team2_EIS.Controllers
+{
+    public class ProduceController : Controller
+    {
+        // GET: Produce
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
