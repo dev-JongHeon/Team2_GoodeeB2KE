a61116ab184edc77554dcd10857f4fd638c276d4
esp7789 <gjwlstjd203@naver.com>
2020-02-20 13:54:14 +0900

    유효성 버그 수정

--- Team2_Shop/Models/ShipInfo.cs
+++ Team2_Shop/Models/ShipInfo.cs
@@ -10,10 +10,12 @@ namespace Team2_Shop.Models
     {
         public int CustomerID { get; set; }
 
-        [Required(ErrorMessage = "주소1을 입력해주세요.")]
-        [Display(Name = "배송지 주소")]
+        [Required(ErrorMessage = "주소를 입력해주세요.")]
+        [Display(Name = "배송지 주소")]        
         public string Addr1 { get; set; }
+
         [Display(Name = "상세주소")]
+        [Required(ErrorMessage = "상세주소를 입력해주세요.")]
         public string Addr2 { get; set; }
 
         [Display(Name ="배송확인 이메일")]
